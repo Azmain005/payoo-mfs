96765ce4db1dd6cc25471a53ec0eeded44dc8784
Azmain005 <azmain.adib.arnob@gmail.com>
2025-04-03 23:25:24 +0600

    added the login feature

--- js/login.js
+++ js/login.js
@@ -1,7 +1,23 @@
-document.getElementById('loginbtn').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    console.log('Login button clicked');
+// document.getElementById('loginbtn').addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent the default form submission behavior
+//     // console.log('Login button clicked');
+//     const phoneNumber = document.getElementById('mobile').value;
+//     const password = document.getElementById('pin').value;
+//     if (phoneNumber === '01718' && password === '123456') {
+//         console.log('Login successful!');
+//     } else {
+//         alert('Wrong mobile number or pin!');
+//     }
+
+// });
+
+document.getElementById('loginbtn').addEventListener('click', function(event) { 
+    event.preventDefault();
     const phoneNumber = document.getElementById('mobile').value;
     const password = document.getElementById('pin').value;
-    
-});
+    if (phoneNumber === '01718' && password === '123456') {
+        window.location.href = 'home.html'; // Redirect to home.html on successful login
+    } else {
+        alert('Wrong mobile number or pin!');
+    }
+})
